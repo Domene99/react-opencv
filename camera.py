e68fa4e6af1b7eabc5f6819b1d38d06696a9a088
Domene99 <jadomene99@gmail.com>
2020-10-06 18:59:25 -0500

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,8 +1,10 @@
 class VideoCamera(object):
     def __init__(self):
       self.video = cv2.VideoCapture(0)
+
     def __del__(self):
       self.video.release()
+
     def get_frame(self):
       ret, frame = self.video.read()
       ret, jpeg = cv2.imencode('.jpg', frame)
