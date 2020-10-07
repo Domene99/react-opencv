f72de2158d4a2b63ef0b4e4c718ced5e5251e1dd
Domene99 <jadomene99@gmail.com>
2020-10-06 19:00:51 -0500

    Create index.js

---
+++ templates/index.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+const Cam = () => {
+return (
+  <div>
+   <img
+    src="http://localhost:5000/video_feed"
+    alt="Video"
+   />
+  </div>
+ );
+};
+
+export default Cam;
